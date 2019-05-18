467048eed9df3d98fc959834f199da4fbd0426af
Daniel <danielgulic@gmail.com>
2019-05-19 01:53:15 +1000

    Use my Prettier config when formatting

--- src/prettify.ts
+++ src/prettify.ts
@@ -3,7 +3,8 @@ import prettier from 'prettier'
 // Formats the given content for the given file using the given Prettier configuration.
 export function prettify(text: string, filename: string, config) {
   const options = { filepath: filename }
-  const merged = { ...config, ...options }
+  const prettierConfig = { singleQuote: true, jsxSingleQuote: true, arrowParens: 'always' } 
+  const merged = { ...config, ...options, ...prettierConfig }
   try {
     return prettier.format(text, merged)
   } catch (e) {
